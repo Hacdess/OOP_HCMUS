224999ed314852bc006d2c8f9af53ca5da0abc33
TKin <letrungkienthd@gmail.com>
2024-10-28 23:47:40 +0700

    modifying: learning

--- GDIplus/LearnGDIplus/LearnGDIplus.cpp
+++ GDIplus/LearnGDIplus/LearnGDIplus.cpp
@@ -4,4 +4,7 @@ void onPaint(HDC hdc)
 {
 	Graphics graphics(hdc);
 	Pen pen(Color(255, 0, 0, 255));
+	graphics.DrawLine(&pen, 0, 0, 200, 100);
 }
+
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
